9ca45eeb34206ad0a5804c5468bed62a9387e35e
elias <elias.w.wester@gmail.com>
2022-09-26 21:35:25 +0200

    Initial commit

---
+++ src/main/java/net/noo1/nodes/NodesClient.java
@@ -0,0 +1,10 @@
+package net.noo1.nodes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NodesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
